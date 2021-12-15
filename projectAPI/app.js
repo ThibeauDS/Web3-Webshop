875ccbd7c2edcf6975bc052d1d026e261b24d241
ThibeauDS <thibeau.desmet@student.hogent.be>
2021-12-15 14:39:09 +0100

    Update 15/12
    Toevoegen van CORS

--- projectAPI/app.js
+++ projectAPI/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const productsRouter = require("./routes/products");
 
@@ -13,8 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(cors());
 app.use("/products", productsRouter);
 
-console.log("API running at port 4000");
+console.log("API is running at port 4000");
 
 module.exports = app;
